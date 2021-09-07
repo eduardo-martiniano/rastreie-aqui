268bd263f71f44e2bd5807c04c4aa57ebb14d6c8
eduardo-martiniano <eeduardo.martiniano@gmail.com>
2021-09-07 09:09:00 -0300

    aplicando flex-box e melhorando layout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
+  @Input() showBackButton = true;
   constructor(private route: Router) { }
 
   ngOnInit(): void {
